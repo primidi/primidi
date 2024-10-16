1f16d2fbbc8db9083e86a2b8d36b9a45c2f0cf0e
Gharis Primada Hariyono <gharis.hariyono@komunal.co.id>
2024-10-16 21:12:06 +0700

    [ignore] remove base config astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://primidi.github.io',
-  base: 'primidi.github.io',
   output: 'static',
 });
