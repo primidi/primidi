aeeb382a14a276b96550a8a020fa89fa428df9d2
Gharis Primada Hariyono <gharis.hariyono@komunal.co.id>
2024-10-16 20:36:44 +0700

    [ignore] try fixing styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-  ],
+  integrations: [tailwind()],
   site: 'https://primidi.github.io',
   base: 'primidi.github.io',
   output: 'static',
