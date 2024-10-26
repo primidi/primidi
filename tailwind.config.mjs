d0ca49d2ee0be195e80e8a6a4ee0543ef8450198
Gharis Primada Hariyono <gharis.hariyono@komunal.co.id>
2024-10-26 10:36:03 +0700

    [ignore] fix tab prettier

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,36 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				pink: {
-					100: '#BA6EB1',
-				},
-				purple: {
-					100: '#9A0680',
-					300: '#79018C',
-					500: '#4C0070',
-					700: '#160040',
-				},
-				grey: {
-					100: '#DBD8E3',
-					500: '#5C5470',
-					700: '#352F44',
-				},
-				brand: {
-					github: '#333333',
-					instagram: '#C13584',
-					x: '#14171A',
-					linkedin: '#0A66C2',
-					spotify: '#1DB954',
-				},
-			},
-			fontFamily: {
-				sans: ['Poppins', 'system-ui'],
-				serif: ['Merriweather', 'serif'],
-			},
-		},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        pink: {
+          100: '#BA6EB1',
+        },
+        purple: {
+          100: '#9A0680',
+          300: '#79018C',
+          500: '#4C0070',
+          700: '#160040',
+        },
+        grey: {
+          100: '#DBD8E3',
+          500: '#5C5470',
+          700: '#352F44',
+        },
+        brand: {
+          github: '#333333',
+          instagram: '#C13584',
+          x: '#14171A',
+          linkedin: '#0A66C2',
+          spotify: '#1DB954',
+        },
+      },
+      fontFamily: {
+        sans: ['Poppins', 'system-ui'],
+        serif: ['Merriweather', 'serif'],
+      },
+    },
+  },
+  plugins: [],
 };
