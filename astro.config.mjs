b894f5746cc83558bb5b33db050b4ef526989f5f
Gharis Primada Hariyono <gharis.hariyono@komunal.co.id>
2024-10-27 16:23:22 +0700

    fix: deployment SSR to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import { remarkReadingTime } from './src/remark-reading-time.mjs';
-
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://primidi.github.io',
+  site: 'https://primidi.vercel.app',
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
