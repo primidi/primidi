0ad4a853bd568de770360f6016997b490838e74e
Gharis Primada Hariyono <gharis.hariyono@komunal.co.id>
2024-10-27 15:33:24 +0700

    features: post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import { remarkReadingTime } from './src/remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://primidi.github.io',
-  output: 'static',
+  output: 'server',
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 });
