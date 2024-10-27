fe358a8443f73b0c87fb3de181e723e2528a7845
Gharis Primada Hariyono <89778042+primidi@users.noreply.github.com>
2024-10-27 15:49:40 +0700

    fix: adapter for SSR (#4)
    Co-authored-by: Gharis Primada Hariyono <gharis.hariyono@komunal.co.id>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import { remarkReadingTime } from './src/remark-reading-time.mjs';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://primidi.github.io',
   output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
