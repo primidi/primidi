72198a319e3d4390cb681d4e088d3387da1457c2
Gharis Primada Hariyono <gharis.hariyono@komunal.co.id>
2024-10-28 14:48:11 +0700

    chores: SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from '@astrojs/tailwind';
 import { remarkReadingTime } from './src/remark-reading-time.mjs';
 import vercel from '@astrojs/vercel/serverless';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   site: 'https://primidi.vercel.app',
   output: 'server',
   adapter: vercel(),
